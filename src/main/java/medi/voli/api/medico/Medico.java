044762ff48d21b84611ae7591b0ad21f7b0f5e45
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-01-06 16:48:15 -0300

    criacao entidades medico e endereco

---
+++ src/main/java/medi/voli/api/medico/Medico.java
@@ -0,0 +1,27 @@
+package medi.voli.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import medi.voli.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+	private String crm;
+	@Enumerated(EnumType.STRING)
+	private Especialidade especialidade;
+	@Embedded
+	private Endereco endereco;
+}
