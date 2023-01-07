e87f9239a1c160ccdc1c86951aab2292986976c7
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-01-07 10:43:17 -0300

    alteracao no dos parametros url

--- src/main/java/medi/voli/api/controller/MedicoController.java
+++ src/main/java/medi/voli/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package medi.voli.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +31,8 @@ public class MedicoController {
 	}
 	
 	@GetMapping
-	public Page<DadosListagemMedico> listar(Pageable paginacao){
+	public Page<DadosListagemMedico> listar(@PageableDefault(size=10, sort = {"nome"}) Pageable paginacao){
+		//o spring tem um pageable padrão, a anotação pageable default vai apenas mudar esse padrão.
 		return repository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
 }
