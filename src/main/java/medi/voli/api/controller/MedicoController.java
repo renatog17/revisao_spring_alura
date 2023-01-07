29998c45b12984991f3c699692f5b317e89babaf
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-01-07 08:09:34 -0300

    get medicos com paginacao

--- src/main/java/medi/voli/api/controller/MedicoController.java
+++ src/main/java/medi/voli/api/controller/MedicoController.java
@@ -1,8 +1,8 @@
 package medi.voli.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +30,7 @@ public class MedicoController {
 	}
 	
 	@GetMapping
-	public List<DadosListagemMedico> listar(){
-		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+	public Page<DadosListagemMedico> listar(Pageable paginacao){
+		return repository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
 }
