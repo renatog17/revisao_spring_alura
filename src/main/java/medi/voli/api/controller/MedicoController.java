8e4def21b71c8b46fb4ecb522e4b98c11cac42be
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-01-08 10:18:55 -0300

    exclusao logica

--- src/main/java/medi/voli/api/controller/MedicoController.java
+++ src/main/java/medi/voli/api/controller/MedicoController.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,5 +45,11 @@ public class MedicoController {
 		var medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
 		// não será preciso fazer mais nada para salvar os dados do médico, pois este método está dentro de uma transação
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluir(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
